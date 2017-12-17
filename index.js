73743c08d4f4b267a7efb8d05a9db3074aef3a55
jeremylorino@gmail.com <jeremylorino@gmail.com>
2017-12-17 07:30:09 +0000

    add refactor Warning header parser. add Api Warning header parser

--- index.js
+++ index.js
@@ -1,27 +1,94 @@
-module.exports = function parseWarningHeader (header) {
-  if (!header || typeof header !== 'string') return []
-  var warnings = header.split(/([0-9]{3} [a-z0-9.@\-\/]*) /g)
+'use strict';
 
-  var previous
-  function generateObject (all, w) {
-    w = w.trim()
-    var newError = w.match(/^([0-9]{3}) (.*)/)
-    if (newError) previous = {code: newError[1], agent: newError[2]}
-    else if (w) {
-      var errorContent = w.split(/" "/)
-      if (errorContent) {
-        previous.message = strip(errorContent[0])
-        previous.date = strip(errorContent[1])
-        all.push(previous)
-      }
-    }
-    return all
+const warningHeaderRegex = /^(\d{3})\s([\[\]a-z0-9.@\-\/\:]*)\s\"(.+?)\"(?:\s\"(.+?)\")?$/;
+const apiWarningHeaderRegex = /^(\d{3}),\s(.+?),\s(.+?),\s(.+?)$/;
+
+class WarningHeader {
+  constructor() {
+    this.code = null;
+    this.agent = null;
+    this.message = null;
+    this.date = null;
+  }
+  
+  /**
+   * Whether the parsed value is a valid Warning header.
+   * 
+   * @returns {Boolean}
+   */
+  get valid() {
+    return Object.is(this, new this.constructor());
   }
 
-  return warnings.reduce(generateObject, [])
+  /**
+   * Tries to parse a String according to the Warning header format.
+   * 
+   * @param {String} headerValue - Warning header value.
+   * @returns {WarningHeader}
+   * 
+   * @example
+   * let warning = WarningHeader.parseValue(`112 - "network down" "Sat, 25 Aug 2012 23:34:45 GMT"`));
+   * console.log(warning);
+   * // WarningHeader {
+   * //  code: '112',
+   * //  agent: '-',
+   * //  message: 'network down',
+   * //  date: 'Sat, 25 Aug 2012 23:34:45 GMT'
+   * // }
+   */
+  static parseValue(headerValue) {
+    let matches = warningHeaderRegex.exec(headerValue);
+    matches = Array.isArray(matches) ? matches.splice(1) : [];
+    
+    let warning = new this();
+    let keys = Object.keys(warning);
+    matches.reduce((obj, value, index)=>(obj[keys[index]] = value, obj), warning);
+    
+    return warning;
+  }
 }
 
-function strip (s) {
-  if (!s) return
-  return s.replace(/(^"|[",]*$)/, '')
+module.exports.WarningHeader = WarningHeader;
+
+class ApiWarningHeader extends WarningHeader {
+  constructor() {
+    super();
+    
+    this.api = {
+      code: null,
+      text: null,
+      detailUri: null,
+      date: null,
+    };
+  }
+
+  /**
+   * Tries to parse a String according to the Warning header format.
+   * 
+   * @param {String} headerValue - Warning header value.
+   * @returns {WarningHeader}
+   * 
+   * @example
+   * let warning = WarningHeader.parseValue(`112 - "network down" "Sat, 25 Aug 2012 23:34:45 GMT"`));
+   * console.log(warning);
+   * // WarningHeader {
+   * //  code: '112',
+   * //  agent: '-',
+   * //  message: 'network down',
+   * //  date: 'Sat, 25 Aug 2012 23:34:45 GMT'
+   * // }
+   */
+  static parseValue(headerValue) {
+    let warning = super.parseValue(headerValue);
+    
+    let matches = apiWarningHeaderRegex.exec(warning.message);
+    matches = Array.isArray(matches) ? matches.splice(1) : [];
+    
+    let keys = Object.keys(warning.api);
+    matches.reduce((obj, value, index)=>(obj[keys[index]] = value, obj), warning.api);
+    
+    return warning;
+  }
 }
+
+module.exports.ApiWarningHeader = ApiWarningHeader;
--- test.js
+++ test.js
@@ -1,23 +1,83 @@
-var assert = require('assert')
-var parse = require('./')
+'use strict';
+const assert = require('assert'),
+  {
+    WarningHeader,
+    ApiWarningHeader,
+  } = require('./');
 
-var warning = parse('199 agent "Error message" "2015-01-01"')
-assert.equal(warning.length, 1)
-var w = warning[0]
-assert.deepEqual(w, {code: '199', agent: 'agent', message: 'Error message', date: '2015-01-01'})
+let warning = WarningHeader.parseValue('199 agent "Error message" "2015-01-01"');
+assert(warning instanceof WarningHeader, true);
+assert.equal(Object.keys(warning).length, 4);
+assert.deepEqual(warning, {
+  code: '199',
+  agent: 'agent',
+  message: 'Error message',
+  date: '2015-01-01'
+});
 
-var warnings = parse('199 agent "Error message" "2015", 299 agent2 "Error message 2 with \\" quote" "2016"')
-assert.equal(warnings.length, 2)
-assert.deepEqual(warnings[0], {code: '199', agent: 'agent', message: 'Error message', date: '2015'})
-assert.deepEqual(warnings[1], {code: '299', agent: 'agent2', message: 'Error message 2 with \\" quote', date: '2016'})
+// TODO: remove this test. format is not part of [RFC 7234 §5.5](https://tools.ietf.org/html/rfc7234#section-5.5)
+// let warnings = WarningHeader.parseValue('199 agent "Error message" "2015", 299 agent2 "Error message 2 with \\" quote" "2016"');
+// assert.equal(warnings.length, 2);
+// assert.deepEqual(warnings[0], {code: '199', agent: 'agent', message: 'Error message', date: '2015'});
+// assert.deepEqual(warnings[1], {code: '299', agent: 'agent2', message: 'Error message 2 with \\" quote', date: '2016'});
 
 // HTTP-Date format support https://tools.ietf.org/html/rfc7231#section-7.1.1.1
-var httpDate = parse('199 agent "Error message" "Sun, 06 Nov 1994 08:49:37 GMT"')
-assert.deepEqual(httpDate[0], {code: '199', agent: 'agent', message: 'Error message', date: 'Sun, 06 Nov 1994 08:49:37 GMT'})
+let httpDate = WarningHeader.parseValue('199 agent "Error message" "Sun, 06 Nov 1994 08:49:37 GMT"');
+assert.deepEqual(httpDate, {
+  code: '199',
+  agent: 'agent',
+  message: 'Error message',
+  date: 'Sun, 06 Nov 1994 08:49:37 GMT'
+});
+
+warning = WarningHeader.parseValue(`112 - "network down" "Sat, 25 Aug 2012 23:34:45 GMT"`);
+assert.deepEqual(warning, {
+  code: '112',
+  agent: '-',
+  message: 'network down',
+  date: 'Sat, 25 Aug 2012 23:34:45 GMT'
+});
+
+warning = WarningHeader.parseValue(`299 google.com "300, Deprecation, https://yoursite.com/details/300.json, 2017-12-12T23:28:18.508Z" "Wed, 21 Oct 2015 07:28:00 GMT"`);
+assert.deepEqual(warning, {
+  code: '299',
+  agent: 'google.com',
+  message: '300, Deprecation, https://yoursite.com/details/300.json, 2017-12-12T23:28:18.508Z',
+  date: 'Wed, 21 Oct 2015 07:28:00 GMT'
+});
+
+warning = WarningHeader.parseValue(`299 google.com:80 "300, Deprecation, https://yoursite.com/details/300.json, 2017-12-12T23:28:18.508Z" "Wed, 21 Oct 2015 07:28:00 GMT"`);
+assert.deepEqual(warning, {
+  code: '299',
+  agent: 'google.com:80',
+  message: '300, Deprecation, https://yoursite.com/details/300.json, 2017-12-12T23:28:18.508Z',
+  date: "Wed, 21 Oct 2015 07:28:00 GMT"
+});
+
+warning = WarningHeader.parseValue(`299 2001:1900:3001:11::3e/a "300, Deprecation, https://yoursite.com/details/300.json, 2017-12-12T23:28:18.508Z" "Wed, 21 Oct 2015 07:28:00 GMT"`);
+assert.deepEqual(warning, {
+  code: '299',
+  agent: '2001:1900:3001:11::3e/a',
+  message: '300, Deprecation, https://yoursite.com/details/300.json, 2017-12-12T23:28:18.508Z',
+  date: 'Wed, 21 Oct 2015 07:28:00 GMT'
+});
 
 // Returns an empty array for invalid headers
-assert.equal(Array.isArray(parse()), true)
-assert.equal(parse().length, 0)
+assert.equal(WarningHeader.parseValue().valid, false);
+assert.equal(WarningHeader.parseValue('').valid, false);
 
-assert.equal(Array.isArray(parse('')), true)
-assert.equal(parse('').length, 0)
+warning = ApiWarningHeader.parseValue(`299 2001:1900:3001:11::3e/a "300, Deprecation, https://yoursite.com/details/300.json, 2017-12-12T23:28:18.508Z" "Wed, 21 Oct 2015 07:28:00 GMT"`);
+assert.deepEqual(warning, {
+  code: '299',
+  agent: '2001:1900:3001:11::3e/a',
+  message: '300, Deprecation, https://yoursite.com/details/300.json, 2017-12-12T23:28:18.508Z',
+  date: 'Wed, 21 Oct 2015 07:28:00 GMT',
+  api: {
+    code: '300',
+    text: 'Deprecation',
+    detailUri: 'https://yoursite.com/details/300.json',
+    date: '2017-12-12T23:28:18.508Z',
+  }
+});
+
+console.log('complete');
